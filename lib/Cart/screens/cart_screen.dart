0f4246fea624df5d4a8bbc8551a1a9f8d813a599
meeramohandas <me.meeramohandas@gmail.com>
2024-08-18 17:15:14 +0530

    "Replace Row with Flex in CartScreen widget"

--- lib/Cart/screens/cart_screen.dart
+++ lib/Cart/screens/cart_screen.dart
@@ -76,8 +76,9 @@ class CartScreen extends StatelessWidget {
                     Padding(
                       padding: const EdgeInsets.only(
                           left: 12.0, top: 8, bottom: 8, right: 12),
-                      child: Row(
-                        crossAxisAlignment: CrossAxisAlignment.center,
+                      child: Flex(
+                        direction: Axis.horizontal,
+                        mainAxisAlignment: MainAxisAlignment.start,
                         children: [
                           Container(
                             width: imageSize,
